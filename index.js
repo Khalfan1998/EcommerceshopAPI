fe49dabc5fe389c8b4134328d442eb24223683aa
Khalfan1998 <khalfan.ikns@gmail.com>
2022-01-05 13:10:04 +0300

    Routes
    adding routes

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
+const authRoute = require("./routes/auth");
+const productRoute = require("./routes/product");
+
+dotenv.config();
 
 mongoose
-  .connect(
-    "mongodb+srv://user:1234@cluster0.gclpy.mongodb.net/EShop?retryWrites=true&w=majority"
-  )
+  .connect(process.env.MONGO_URL)
   .then(() => console.log("DB Connection Successfull"))
   .catch((err) => {
     console.log(err);
   });
 
-app.listen(5000, () => {
+app.use(express.json());
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
+
+app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
 });
